db67ad2a59b67e5804b79806c50d96a7f3026095
Veronazavr <verochka10@list.ru>
2020-12-09 20:36:18 +0300

    added updated

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturn100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+
+
+    @Test
+    public void shouldReturn0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+}
