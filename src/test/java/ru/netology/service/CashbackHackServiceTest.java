24ad05c04f11e668c46c2fc85a986bec7359c977
Veronazavr <verochka10@list.ru>
2020-12-13 12:59:16 +0300

    added updated Test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,8 @@
 package ru.netology.service;
 
-import org.junit.Assert;
-import org.junit.Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
 
@@ -21,6 +22,17 @@ public class CashbackHackServiceTest {
     public void shouldReturn0IfAmountIs900() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
 
         int actual = service.remain(amount);
         int expected = 0;
