bb902e460d292067a6c73bfd1c37f815de958624
Veronazavr <verochka10@list.ru>
2020-12-14 17:15:57 +0300

    added updated Test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -18,10 +18,10 @@ public class CashbackHackServiceTest {
 
 
     @Test
-    public void shouldReturn10() {
+    public void shouldReturn0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-        int expected = 10;
+        int expected = 0;
         int actual = service.remain(amount);
         assertEquals(expected, actual);
     }
